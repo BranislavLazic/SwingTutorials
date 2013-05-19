fd71a68b36ff347ebd5d074ccd8042bccdee55e4
BranislavLazic <brano2411@hotmail.com>
2013-05-19 23:00:13 +0200

    Added new JPasswordField

--- src/JPasswordFieldTutorial.java
+++ src/JPasswordFieldTutorial.java
@@ -1,29 +1,29 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.SwingUtilities;
-
+/**
+ * Created with IntelliJ IDEA.
+ * User: Branislav
+ * Date: 5/19/13
+ * Time: 3:56 PM
+ */
 public class JPasswordFieldTutorial {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JPasswordField passwordField = new JPasswordField(20);
-	JButton button = new JButton("Login");
+	JButton button = new JButton("Perform check");
 
 	public JPasswordFieldTutorial() {
 		button.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				char[] input = passwordField.getPassword();
-				if (performCheck(input)) {
-					JOptionPane.showMessageDialog(null, "You just logged in!");
+			public void actionPerformed(ActionEvent e) {
+				char [] input = passwordField.getPassword();
+				if(checkIfCorrect(input)) {
+					JOptionPane.showMessageDialog(null,"Password is correct!");
 				} else {
-					JOptionPane.showMessageDialog(null, "Failed to log in");
+					JOptionPane.showMessageDialog(null,"Password is incorrect!");
 				}
 			}
 		});
@@ -37,21 +37,20 @@ public class JPasswordFieldTutorial {
 		frame.setVisible(true);
 	}
 
-	private boolean performCheck(char[] input) {
+	private boolean checkIfCorrect(char [] input) {
 		boolean isCorrect = false;
-		char[] correctPass = { '1', '2', '3' };
-		if (input.length != correctPass.length) {
-			isCorrect = false;
-		}
-		if (Arrays.equals(input, correctPass)) {
-			isCorrect = true;
-		}
-		Arrays.fill(correctPass, '0');
+		char [] correctPass = {'1','2','3'};
 
+		if(input.length != correctPass.length) {
+			isCorrect = false;
+		} else {
+			isCorrect = Arrays.equals(input, correctPass);
+		}
+		Arrays.fill(correctPass,'0');
 		return isCorrect;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
